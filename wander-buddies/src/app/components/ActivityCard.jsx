f6b244156359f6a50ea06c56d7a6594230d9cff5
zhanginc <zhanginc@students.cs.ubc.ca>
2025-03-09 01:56:05 -0800

    adds actiivty dashboard

--- wander-buddies/src/app/components/ActivityCard.jsx
+++ wander-buddies/src/app/components/ActivityCard.jsx
@@ -3,7 +3,7 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Box from '@mui/material/Box';
 import { CardMedia, Typography, CardActions } from '@mui/material';
-import { StyledAvatar } from './StyledAvatar';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 export const ActivityCard = ({sizeLarge = true}) => {
     return(
@@ -42,41 +42,43 @@ export const ActivityCard = ({sizeLarge = true}) => {
 
             </Box>
             <CardActions sx={{display: "flex", justifyContent: "center"}}>
-                     <CardMedia
-                        sx={{ height: 50, width: 50, justifyContent: "center"}}
-                        image="/public/images/icons/arrow.svg"
-                        title="person one"
-                    />
-
-
+                     <ArrowForwardIosIcon />
              </CardActions>
+
       </CardContent>
     </Card> : 
-    <Card variant="outlined" sx={{borderRadius: "10px", width: "100%", background:"#64A181"}}>
-        <CardContent sx={{display: "grid", gridTemplateColumns: "0.5fr 2fr 1fr", textAlign: "left", alignItems: "center"}}>
-                <Box sx={{display:"flex", justifyContent: "center", textAlign: "center" }}>
-                    <Typography sx={{ color: 'text.primary', fontSize: 20 }}>
-                        Jan 8
+    <Card variant="outlined" sx={{borderRadius: "20px", width: "100%", height: "130px", background:"#64A181", display: "grid", gridTemplateColumns: "1fr 2fr 1.5fr", textAlign: "left", alignItems: "center"}}>
+                <Box sx={{
+                        background: 'white',
+                        height: "100%",
+                        width: "100%",
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        borderRadius: "10px",
+                    }}>
+                    <Typography sx={{ fontSize: 20 }}>
+                        Jan 
+                    </Typography>
+                    <Typography sx={{ fontSize: 20 }}>
+                        8
                     </Typography>
 
-                </Box>                
-            <Box sx={{display:"flex", flexDirection: "column", marginLeft: 3, justifyContent: "center"}}>
-                <Typography sx={{ color: 'text.primary', fontSize: 20}}>
-                    Acitivy Name
-                </Typography>
-                <Typography sx={{ color: 'text.secondary', fontSize: 14 }}>
-                    Location
-                </Typography>
 
-            </Box>
+                </Box>                
+                <CardContent sx={{display: 'flex', flexDirection: 'column', textAlign: "left" }}>
+                    <Typography sx={{ color: 'white', fontSize: 20, fontWeight: 'bold' }}>
+                        Activity Name
+                    </Typography>
+                        <Typography sx={{ color: 'black', fontSize: 14, fontWeight: 'bold' }}>
+                        Location
+                        </Typography>
+                </CardContent>
             <CardActions sx={{display: "flex", justifyContent: "flex-end", alignItems: "flex-end"}}>
-                     <CardMedia
-                        sx={{ height: 20, width: 20, justifyContent: "center"}}
-                        image="/public/images/icons/arrow.svg"
-                        title="person one"
-                    />
+                <ArrowForwardIosIcon sx={{color: "white"}} />
              </CardActions>
-      </CardContent>
+      
     </Card>
         }
     </Box>
